05a0c28bb849a9de44993ae23ef5304d1017a713
Ben <bfong000@gmail.com>
2024-04-06 21:52:51 -0700

    frontend current state

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div style={{ fontWeight: 'bold', fontFamily: 'Helvetica, sans-serif', padding: '10px', textAlign: 'center', backgroundColor: '#646464' }}>
+            SFMTA GARAGES
+        </div>
+    )
+}
+export default Header
